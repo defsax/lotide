4f6852abfde7fea7aecae183d0335e85ba76a120
Perry Defayette <defayetteperry@gmail.com>
2021-01-04 21:53:42 +0000

    Added solutions.

--- tail.js
+++ tail.js
@@ -6,11 +6,28 @@ const assertEqual = function(a, b) {
 };
 
 const tail = function(arr) {
-  return new Array(arr.slice(1));
+  return arr.slice(1);
 };
 
 //test, returns array less 'a'
 console.log(tail(['a', 'b', 'c', 'd']));
 
-const result = tail(['Hello', 'Lighthouse', 'Labs']);
+//set up arrays
+const words = ['Hello', 'Lighthouse', 'Labs'];
+const result = tail(words);
+
+//this will fail because we cannot directly compare two arrays
 assertEqual(result, ['Lighthouse', 'Labs']);
+
+//compass solution is hardcoded...
+assertEqual(result.length, 2);
+assertEqual(result[0], 'Lighthouse');
+assertEqual(result[1], 'Labs');
+
+//original array is unmodified
+console.log(words);
+
+//test: returns empty array
+console.log(tail(['a']));
+//test: empty array return empty array
+console.log(tail([]));
