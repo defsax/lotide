f7850db3ea9a65a6355b5b60006370c49559a2ba
Perry Defayette <defayetteperry@gmail.com>
2021-01-05 21:55:33 +0000

    Updated eqArrays and assertArraysEqual scripts

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,9 @@
 const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    console.log('Arrays are not same length.');
+    return false;
+  }
+
   for (let i = 0; i < arrayA.length; i++) {
     console.log(arrayA[i], arrayB[i]);
     if (arrayA[i] !== arrayB[i])
@@ -6,3 +11,16 @@ const eqArrays = function(arrayA, arrayB) {
   }
   return true;
 };
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB))
+    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}`);
+  else
+    console.log(`❌ Assertion failed: ${arrayB} !== ${arrayB}`);
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,21 @@
 const assertEqual = function(a, b) {
   if (a === b)
-    console.log(`✅ Assertion passed: ${a} === ${b}`);
+    console.log(`✅ Assertion passed: ${a} === ${b}\n`);
   else
-    console.log(`❌ Assertion failed: ${a} !== ${b}`);
+    console.log(`❌ Assertion failed: ${a} !== ${b}\n`);
 };
 
 const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    console.log('Arrays are not same length.');
+    return false;
+  }
+  
   for (let i = 0; i < arrayA.length; i++) {
-    console.log(arrayA[i], arrayB[i]);
-    if (arrayA[i] !== arrayB[i])
+    if (arrayA[i] !== arrayB[i]) {
+      console.log('Elements ' + arrayA[i] + ' and ' + arrayB[i] + ' are not equal.');
       return false;
+    }
   }
   return true;
 };
@@ -19,3 +25,6 @@ assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
 
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
+
+assertEqual(eqArrays(["a", "b", "c"], ["a", "b", "c"]), true); // => false
+assertEqual(eqArrays(["a", "b", "c"], ["c", "b", "a"]), true); // => false
