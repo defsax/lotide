5970e12208e771e634773b72629772fe2427d742
Perry Defayette <defayetteperry@gmail.com>
2021-01-06 20:14:37 +0000

    Finished findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(a, b) {
+  if (a === b)
+    console.log(`✅ Assertion passed: ${a} === ${b}`);
+  else
+    console.log(`❌ Assertion failed: ${a} !== ${b}`);
+};
+
+const findKeyByValue = function(obj, val) {
+  for (let i in obj) {
+    if (obj[i] === val)
+      return i;
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  // eslint-disable-next-line camelcase
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
