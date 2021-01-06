09972894964f73947fa0f45322916eb83a031c60
Perry Defayette <defayetteperry@gmail.com>
2021-01-06 17:36:41 +0000

    Finished middle.js

---
+++ middle.js
@@ -0,0 +1,44 @@
+const eqArrays = function(arrayA, arrayB) {
+  if (arrayA.length !== arrayB.length) {
+    console.log('Arrays are not same length.');
+    return false;
+  }
+
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== arrayB[i]) {
+      console.log('Elements ' + arrayA[i] + ' and ' + arrayB[i] + ' are not equal or are different types.');
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arrayA, arrayB) {
+  if (eqArrays(arrayA, arrayB))
+    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`);
+  else
+    console.log(`❌ Assertion failed: ${arrayA} !== ${arrayB}\n`);
+};
+
+const middle = function(arr) {
+  if (arr.length < 3) //no middle
+    return [];
+  else if (arr.length % 2 === 0) {
+    //even
+    //console.log('even array:', arr, 'array middle:', Math.floor(arr.length / 2));
+    return [arr[Math.floor(arr.length / 2) - 1], arr[Math.floor(arr.length / 2)]];
+  } else {
+    //odd
+    return [arr[Math.floor(arr.length / 2)]];
+  }
+};
+
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
