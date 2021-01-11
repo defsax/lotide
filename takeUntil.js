21f75e7bfaf84128c2866bbc4f8c8605154225e3
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 20:07:32 +0000

    Refactored takeUnil.js

--- takeUntil.js
+++ takeUntil.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    console.log('Arrays are not same length.');
-    return false;
-  }
-
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      console.log('Elements ' + arrayA[i] + ' and ' + arrayB[i] + ' are not equal or are different types.');
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB))
-    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`);
-  else
-    console.log(`❌ Assertion failed: ${arrayA} !== ${arrayB}\n`);
-};
-
 const takeUntil = function(array, callback) {
   let results = [];
   
@@ -34,11 +12,3 @@ const takeUntil = function(array, callback) {
 };
 
 module.exports = takeUntil;
-
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", 'been', 'to', 'Hollywood']);
