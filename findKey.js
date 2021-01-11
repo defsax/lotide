db298dad59bd27f2379bd3a51921d52a34084cbb
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 19:51:33 +0000

    Refactored findKey.js

--- findKey.js
+++ findKey.js
@@ -1,10 +1,3 @@
-const assertEqual = function(a, b) {
-  if (a === b)
-    console.log(`✅ Assertion passed: ${a} === ${b}`);
-  else
-    console.log(`❌ Assertion failed: ${a} !== ${b}`);
-};
-
 const findKey = function(obj, callback) {
   //loop through object
   for (let value in obj) {
@@ -19,25 +12,3 @@ const findKey = function(obj, callback) {
 };
 
 module.exports = findKey;
-
-/*
-  Method using Object.entries courtesy of Luc/Alvin
-
-  const findKey = function(data, callback) {
-    for (let [key,value] of Object.entries(data)) {
-      if (callback(value)) {
-        return key;
-      }
-    }
-  };
-  
-*/
-
-assertEqual(findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2), 'noma'); // => "noma"
