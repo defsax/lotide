ba69342d9beef9c1d53cc5e6c7a3438c803bfd90
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 23:30:09 +0000

    Refactored map test.

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -4,12 +4,14 @@ const assert = require('chai').assert;
 describe('#flatten', function() {
   //finds first key that satisfies callback
   it('returns flat array from [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]]', function() {
+    
     let unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]];
 
     assert.deepEqual(flatten([], unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15.5, 16, 17]);
   });
 
   it('returns flat array from [[10], 1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]]', function() {
+
     let unflattenedArray = [[10], 1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]];
 
     assert.deepEqual(flatten([], unflattenedArray), [10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
--- test/mapTest.js
+++ test/mapTest.js
@@ -1,8 +1,16 @@
 const map = require('../map.js');
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 
-const words = ["ground", "control", "to", "major", "tom"];
-assertArraysEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't']);
+describe('#map', function() {
+  //map test
 
-const nums = [4, 6, 34, 9, 10];
-assertArraysEqual(map(nums, num => num * 2), [8, 12, 68, 18, 20]);
+  it('returns the first letter of each word "ground", "control", "to", "major", "tom"', function() {
+    const words = ["ground", "control", "to", "major", "tom"];
+    assert.deepEqual(map(words, word => word[0]), ['g', 'c', 't', 'm', 't'], "['g', 'c', 't', 'm', 't']");
+  });
+
+  it('doubles the array [4, 6, 34, 9, 10]', function() {
+    const nums = [4, 6, 34, 9, 10];
+    assert.deepEqual(map(nums, num => num * 2), [8, 12, 68, 18, 20]);
+  });
+});
