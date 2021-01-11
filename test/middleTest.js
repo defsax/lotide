f766562fc924ac6e3e59ffe6b6b0cbb9d326425e
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 17:24:42 +0000

    Added middleTest.js

---
+++ test/middleTest.js
@@ -0,0 +1,12 @@
+const middle = require('../middle.js');
+const assertArraysEqual = require('../assertArraysEqual.js');
+
+assertArraysEqual(middle([]), []);
+assertArraysEqual(middle([1]), []);
+assertArraysEqual(middle([1, 2]), []);
+assertArraysEqual(middle([1, 2, 3]), [2]);
+assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7]), [4]);
+assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
