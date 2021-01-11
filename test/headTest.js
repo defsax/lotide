287cc9d67d5265aa2f1c28f409b46a4deafda9b4
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 18:10:56 +0000

    Refactored headTest to incorporate mocha and chai and added a .gitignore.

--- test/headTest.js
+++ test/headTest.js
@@ -1,8 +1,28 @@
 const head = require('../head.js');
-const assertEqual = require('../assertEqual.js');
+const assert = require('chai').assert;
 
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe('#head', function() {
+  it('returns 1 for [1, 2, 3]', function() {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
 
-//head with an empty array
-console.log(head([]));
+  it('returns 5 for [5, 6, 7]', function() {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns '5' for ['5']", function() {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns '5' for ['5', '4', '2']", function() {
+    assert.strictEqual(head(['5', '4', '2,']), '5');
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", function() {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it('returns undefined when passed an empty array', function() {
+    assert.strictEqual(head([]), undefined);
+  });
+});
