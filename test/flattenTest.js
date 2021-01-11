9ce1e393019719e6db6b3ff1e49bfc5c3d402feb
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 23:20:44 +0000

    Wrote flatten tests.

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,12 +1,17 @@
-const assertArraysEqual = require('../assertArraysEqual.js');
 const flatten = require('../flatten.js');
+const assert = require('chai').assert;
 
-let flattenedArray = [];
-let unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]];
+describe('#flatten', function() {
+  //finds first key that satisfies callback
+  it('returns flat array from [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]]', function() {
+    let unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]];
 
-assertArraysEqual(flatten(flattenedArray, unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15.5, 16, 17]); // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15.5, 16, 17]
+    assert.deepEqual(flatten([], unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 15.5, 16, 17]);
+  });
 
-flattenedArray = [];
-unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]];
+  it('returns flat array from [[10], 1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]]', function() {
+    let unflattenedArray = [[10], 1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]];
 
-assertArraysEqual(flatten(flattenedArray, unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
+    assert.deepEqual(flatten([], unflattenedArray), [10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
+  });
+});
