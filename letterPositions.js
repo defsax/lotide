273995102f53494ee08cc80d51e109a2cf29c0ee
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 20:04:16 +0000

    Refactored letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    console.log('Arrays are not same length.');
-    return false;
-  }
-
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      console.log('Elements ' + arrayA[i] + ' and ' + arrayB[i] + ' are not equal or are different types.');
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB))
-    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`);
-  else
-    console.log(`❌ Assertion failed: ${arrayA} !== ${arrayB}\n`);
-};
-
 const letterPositions = function(sentence) {
   sentence = sentence.split(" ").join("");
   let results = {};
@@ -35,11 +13,4 @@ const letterPositions = function(sentence) {
 };
 
 module.exports = letterPositions;
-
-console.log(letterPositions('hello'));
-assertArraysEqual(letterPositions("hello").h, [0]);
-assertArraysEqual(letterPositions("hello").e, [1]);
-assertArraysEqual(letterPositions("hello").l, [2, 3]);
-assertArraysEqual(letterPositions("hello").o, [4]);
-
 
