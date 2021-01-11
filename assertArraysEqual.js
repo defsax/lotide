49a8ec287b851132822044885221d07ed8e26495
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 20:39:46 +0000

    Refactored assertArraysEqual and test.

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,10 +1,13 @@
 const eqArrays = require('./eqArrays.js');
 
 const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB))
-    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`);
-  else
+  if (eqArrays(arrayA, arrayB)) {
+    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`); 
+    return true;
+  } else {
     console.log(`❌ Assertion failed: ${arrayA} !== ${arrayB}\n`);
+    return false;
+  }
 };
 
 module.exports = assertArraysEqual;
--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,21 @@
 const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 
-//assertArraysEqual test code
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
+describe('#assertArraysEqual', function() {
+  //test, returns true or false. prints a message.
+  it("returns true when comparing [1, 2, 3], [1, 2, 3]", function() {
+    assert.isTrue(assertArraysEqual([1, 2, 3], [1, 2, 3]));
+  });
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+  it("returns false when comparing [1, 2, 3], [3, 2, 1]", function() {
+    assert.isFalse(assertArraysEqual([1, 2, 3], [3, 2, 1]));
+  });
+
+  it("returns true when comparing ['1', '2', '3'], ['1', '2', '3']", function() {
+    assert.isTrue(assertArraysEqual(['1', '2', '3'], ['1', '2', '3']));
+  });
+
+  it("returns false when comparing ['1', '2', '3'], ['1', '2', 3]", function() {
+    assert.isFalse(assertArraysEqual(['1', '2', '3'], ['1', '2', 3]));
+  });
+});
