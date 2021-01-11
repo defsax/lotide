93d88aafb4b733455de26269bac954fcc5bf2892
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 20:00:24 +0000

    Refactored flatten.js

--- flatten.js
+++ flatten.js
@@ -1,25 +1,3 @@
-const eqArrays = function(arrayA, arrayB) {
-  if (arrayA.length !== arrayB.length) {
-    console.log('Arrays are not same length.');
-    return false;
-  }
-
-  for (let i = 0; i < arrayA.length; i++) {
-    if (arrayA[i] !== arrayB[i]) {
-      console.log('Elements ' + arrayA[i] + ' and ' + arrayB[i] + ' are not equal or are different types.');
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arrayA, arrayB) {
-  if (eqArrays(arrayA, arrayB))
-    console.log(`✅ Assertion passed: ${arrayA} === ${arrayB}\n`);
-  else
-    console.log(`❌ Assertion failed: ${arrayA} !== ${arrayB}\n`);
-};
-
 const flatten = function(flatArr, unsortedArr) {
   for (let i of unsortedArr) {
     if (Array.isArray(i))
@@ -31,13 +9,3 @@ const flatten = function(flatArr, unsortedArr) {
 };
 
 module.exports = flatten;
-
-let flattenedArray = [];
-let unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15, 15.5], 16, 17]];
-
-assertArraysEqual(flatten(flattenedArray, unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]); // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
-
-flattenedArray = [];
-unflattenedArray = [1, 2, [3, 4], 5, [6], [7, 8, 9, [10, 11, 12, [13, 14]], [15], 16, 17]];
-
-assertArraysEqual(flatten(flattenedArray, unflattenedArray), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]);
