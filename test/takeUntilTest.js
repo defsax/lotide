a1a9e0d31a2cd1ea160e13c3e89cf0830c9ac8ff
Perry Defayette <defayetteperry@gmail.com>
2021-01-11 23:42:21 +0000

    Wrote all tests.

--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,10 +1,15 @@
-const takeUntil = require('../letterPositions.js');
-const assertArraysEqual = require('../assertArraysEqual.js');
+const takeUntil = require('../takeUntil.js');
+const assert = require('chai').assert;
 
-// const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-// assertArraysEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
-
-// console.log('---');
-
-// const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-// assertArraysEqual(takeUntil(data2, x => x === ','), ["I've", 'been', 'to', 'Hollywood']);
+describe('#takeUntil', function() {
+  //takeUntil test
+  it('returns numbers in array until x < 0',function() {
+    const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    assert.deepEqual(takeUntil(data1, x => x < 0), [1, 2, 5, 7, 2]);
+  });
+  
+  it('returns string until comma', function() {
+    const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    assert.deepEqual(takeUntil(data2, x => x === ','), ["I've", 'been', 'to', 'Hollywood'], 'msg');
+  });
+});
--- test/withoutTest.js
+++ test/withoutTest.js
@@ -1,14 +1,29 @@
 const without = require('../without.js');
-const assertArraysEqual = require('../assertArraysEqual.js');
+const assert = require('chai').assert;
 
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+describe('#without', function() {
+  //without test
 
+  it('without doesn\'t mutate array', function() {
+    const words = ["hello", "world", "lighthouse"];
+    without(words, ["lighthouse"]); // no need to capture return value for this test case
+    // Make sure the original array was not altered by the without function
+    assert.deepEqual(words, ["hello", "world", "lighthouse"]);
+  });
+  
+  it('removes 1 from [1, 2, 3]', function() {
+    assert.deepEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
+  });
+  
+  it('removes only type matching elements: ["1", "2", "3"], [1, 2, "3"] = ["1", "2"]', function() {
+    assert.deepEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
+  });
 
-assertArraysEqual(without([1, 2, 3], [1]), [2, 3]); // => [2, 3]
-assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // => ["1", "2"]
-
-assertArraysEqual(without([1, 2, 3, 4, 5], [2]), [1, 3, 4, 5]); // => [2, 3]
-assertArraysEqual(without([1, 2, 3, 4, 5], [2, 4, 5]), [1, 3]); // => [2, 3]
+  it('removes elements from middle of the array', function() {
+    assert.deepEqual(without([1, 2, 3, 4, 5], [2]), [1, 3, 4, 5]); // => [2, 3]
+  });
+  
+  it('removes multiple elements from different positions in array', function() {
+    assert.deepEqual(without([1, 2, 3, 4, 5], [2, 4, 5]), [1, 3]); // => [2, 3]
+  });
+});
