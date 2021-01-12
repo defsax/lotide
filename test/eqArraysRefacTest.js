4b18f68a713bf555943493758f1ff6b72b303c42
Perry Defayette <defayetteperry@gmail.com>
2021-01-12 16:57:00 +0000

    Added additional tests.

--- test/eqArraysRefacTest.js
+++ test/eqArraysRefacTest.js
@@ -15,6 +15,19 @@ describe('#eqArrays', function() {
   it("handles 3 nested arrays", function() {
     assert.isTrue(eqArrays([[1], 2, 3, [4, [5, 6, [7, 8]], 9]], [[1], 2, 3, [4, [5, 6, [7, 8]], 9]]));
   });
+
+  it('handles equal arrays within arrays', function() {
+    assert.isTrue(eqArrays([[2, 3], [4]], [[2, 3], [4]])); // => true
+  });
+
+  it('handles unequal arrays within arrays', function() {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+  });
+
+  it('handles array within array->primitive comparison', function() {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false
+  });
+
   
   it("returns true given ['1', '2', '3'], ['1', '2', '3']", function() {
     assert.isTrue(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
